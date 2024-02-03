d37921f4e1c4cc64d4848a9b2a97ca3adcfcd72d
juanramirezwebdev <juanluisra11@gmail.com>
2024-02-03 20:19:10 +1300

    countries interface

--- src/Countries.tsx
+++ src/Countries.tsx
@@ -1,67 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const Countries: React.FC = () => {
-    const [countries, setCountries] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`https://restcountries.com/v3.1/name/${searchTerm}`);
-                setCountries(response.data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+interface Country {
+  cca2: string;
+  name: {
+    common: string;
+  };
+  currencies: {
+    [key: string]: {
+      name: string;
+    };
+  };
+  languages: {
+    [key: string]: string;
+  };
+}
 
-        
-        if (searchTerm) {
-            fetchData();
-        } else {
-          
-            setCountries([]);
-        }
-    }, [searchTerm]);
+const CountryList: React.FC = () => {
+  const [countries, setCountries] = useState<Country[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
 
-    return (
-        <div>
-            <h1>Country List</h1>
-            <input
-                type="text"
-                placeholder="Enter country name"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
-            <ul>
-                {countries.map((country) => (
-                    <li key={country.cca2}>
-                        <strong>{country.name.common}</strong> -
-                        Currency: {getCurrency(country)} -
-                        Language: {getLanguage(country)}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-};
+  useEffect(() => {
+ 
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Country[]>(`https://restcountries.com/v3.1/name/${searchTerm}`);
+        setCountries(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-const getCurrency = (country) => {
-    const currencies = country.currencies;
-    if (currencies) {
-        const currencyNames = Object.values(currencies).map((currency) => currency.name);
-        return currencyNames.join(', ');
+ 
+    if (searchTerm) {
+      fetchData();
+    } else {
+    
+      setCountries([]);
     }
-    return 'N/A';
+  }, [searchTerm]);
+
+  return (
+    <div>
+      <h1>Country List</h1>
+      <input
+        type="text"
+        placeholder="Enter country name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <ul>
+        {countries.map((country) => (
+          <li key={country.cca2}>
+            <strong>{country.name.common}</strong> - 
+            Currency: {getCurrency(country)} - 
+            Language: {getLanguage(country)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
-const getLanguage = (country) => {
-    const languages = country.languages;
-    if (languages) {
-        const languageNames = Object.values(languages).map((language) => language);
-        return languageNames.join(', ');
-    }
-    return 'N/A';
+
+const getCurrency = (country: Country) => {
+  const currencies = country.currencies;
+  if (currencies) {
+    const currencyNames = Object.values(currencies).map((currency) => currency.name);
+    return currencyNames.join(', ');
+  }
+  return 'N/A';
 };
 
-export default Countries;
+
+const getLanguage = (country: Country) => {
+  const languages = country.languages;
+  if (languages) {
+    const languageNames = Object.values(languages).map((language) => language);
+    return languageNames.join(', ');
+  }
+  return 'N/A';
+};
+
+export default CountryList;
